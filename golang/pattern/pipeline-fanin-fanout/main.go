098f8648ba864b34008e6b2541b089d0c7a82834
cuong pham <cuong.pt0622@outlook.com.vn>
2021-11-15 23:47:44 +0700

    pipeline fanin fanout pattern golang

---
+++ golang/pattern/pipeline-fanin-fanout/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func main() {
+	randomNumbers := []int{}
+	for i := 1; i <= 2; i++ {
+		randomNumbers = append(randomNumbers, i)
+	}
+	// generate pipeline
+	inputChan := generatePipeline(randomNumbers)
+
+	// Fan-out
+	c1 := fanOut(inputChan)
+	c2 := fanOut(inputChan)
+	c3 := fanOut(inputChan)
+	c4 := fanOut(inputChan)
+
+	// Fan-in
+	c := fanIn(c1, c2, c3, c4)
+
+	sum := 0
+	for i := 0; i < len(randomNumbers); i++ {
+		sum += <-c
+	}
+	fmt.Printf("Total sum of square: %d", sum)
+}
+
+func generatePipeline(numbers []int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for _, n := range numbers {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanOut(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func fanIn(inputChannel ...<-chan int) <-chan int {
+	in := make(chan int)
+	go func() {
+		for _, c := range inputChannel {
+			for n := range c {
+				in <- n
+			}
+		}
+	}()
+	return in
+}
