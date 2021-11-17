7e43b0e48f37bc9b386af2fac3cd5c98452a0cde
cuong pham <cuong.pt0622@outlook.com.vn>
2021-11-17 20:54:44 +0700

    worker pool

---
+++ golang/pattern/worker-pool/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	number := 10
+	numberOfWorker := 5
+	jobs := make(chan int, number)
+	results := make(chan int, number)
+	for i := 0; i <= numberOfWorker; i++ {
+		go worker(jobs, results)
+	}
+	for i := 1; i <= number; i++ {
+		jobs <- i
+	}
+	close(jobs)
+
+	for j := 0; j < number; j++ {
+		fmt.Println((<-results))
+	}
+}
+
+func worker(jobs <-chan int, results chan<- int) {
+	for n := range jobs {
+		results <- fib(n)
+	}
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return fib(n-1) + fib(n-2)
+}
